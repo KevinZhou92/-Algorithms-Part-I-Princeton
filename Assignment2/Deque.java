1c6b9ef5e563141b8bdb4968df2a0745fdb1d611
Pengcheng Zhou(Kevin) <monsterzpc@gmail.com>
2016-12-05 04:56:31 +0000

    Assignment2

---
+++ Assignment2/Deque.java
@@ -0,0 +1,115 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+public class Deque<Item> implements Iterable<Item> {
+    private int n; // nodes in linkedlist
+    private Node current;
+    
+    // container for object 
+    private class Node {
+        private Item item;
+        private Node next;
+        private Node previous;
+    }
+    
+    // construct an empty deque
+    public Deque() {
+        current = null;
+        n = 0;
+    }
+    
+    // is the deque empty?
+    public boolean isEmpty() {
+        return current == null;
+    }
+    
+    // return the number of items on the deque
+    public int size() {
+        return n;
+    }  
+    
+    // add the item to the front
+    public void addFirst(Item item) {
+        if(item==null) throw new java.lang.NullPointerException();
+        Node oldfirst = current;
+        current = new Node();
+        current.item = item;
+        current.next = oldfirst;
+        if (current.next != null) {
+          oldfirst.previous = current;
+        }
+        n++;
+    }  
+    
+    // add the item to the end
+    public void addLast(Item item) {
+        if(item==null) throw new java.lang.NullPointerException();
+        if (current == null) {
+            current = new Node();
+            current.item = item;
+        } else {
+            while (current.next != null) current = current.next;
+            Node oldfirst = current;
+            current = new Node();
+            current.item = item;
+            current.previous = oldfirst;
+            oldfirst.next = current;
+            while (current.previous != null) current = current.previous;
+        }    
+        n++;
+    }
+    
+    // remove and return the item from the front
+    public Item removeFirst() {
+        if (current == null) throw new NoSuchElementException();
+        Item temp = current.item;
+        if (current.next != null) {
+            current = current.next;
+            current.previous = null;
+        }
+        else current = null;
+        n--;
+        return temp;
+    }
+    
+    // remove and return the item from the end
+    public Item removeLast() {
+        if (current == null) throw new NoSuchElementException();
+        else if (current.next == null){ 
+            Item temp = current.item;
+            current = null;
+            n--;
+            return temp;
+        }
+        else {
+            while (current.next != null) current = current.next;
+            Item temp = current.item;
+            current = current.previous;   // the last element
+            current.next = null;
+            while (current.previous != null) current = current.previous;
+            n--;
+            return temp;
+        }
+        
+    }
+    
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator() {
+      return new DequeIterator();
+    } 
+    
+    private class DequeIterator implements Iterator<Item> {
+        private Node now = current;
+        public boolean hasNext()  { return now != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+    
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = now.item;
+            now = now.next; 
+            return item;
+        }
+    }
+    
+}
---
+++ Assignment2/RandomizedQueue.java
@@ -0,0 +1,89 @@
+import edu.princeton.cs.algs4.StdRandom;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] a;         //  items
+    private int n;            // number of elements on stack
+    
+    
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        a = (Item[]) new Object[1];    
+        n = 0;
+    }   
+    
+    // is the queue empty?
+    public boolean isEmpty() {
+        return n == 0;
+    }
+    
+    // return the number of items on the queue
+    public int size() {
+        return n;
+    }
+    
+    // enlarge the array by twice of its size
+    private void resize(int capacity) {
+        assert capacity >= n;
+        Item[] temp = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
+            temp[i] = a[i];
+        }
+        a = temp;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+        if (item == null) throw new NullPointerException();
+        if (n == a.length) resize(2*n);
+        a[n++] = item;
+    }      
+    
+    // remove and return a random item
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        int pos =  StdRandom.uniform(n);
+        Item temp = a[pos];
+        a[pos] = a[n-1];
+        a[n-1] = null;
+        n--;
+        if ( n > 0 && n == a.length/4) resize(a.length/2);
+        return temp;
+    }                   
+    
+    // return (but do not remove) a random item
+    public Item sample() {
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        int pos =  StdRandom.uniform(n);
+        Item temp = a[pos];
+        return temp;
+    } 
+     // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator();
+    }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        private int i;
+
+        public RandomizedQueueIterator() {
+            i = n-1;
+        }
+
+        public boolean hasNext() {
+            return i >= 0;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return a[i--];
+        }
+    }
+     
+}
